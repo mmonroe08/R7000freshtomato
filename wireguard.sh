23d1cd115f6bf7960d5d0dc525555372c18e24c0
happy201807 <143249384+happy201807@users.noreply.github.com>
2024-02-20 22:25:55 +0800

    Update wireguard.sh

--- wireguard.sh
+++ wireguard.sh
@@ -4,18 +4,13 @@ ip address add dev wg0 10.7.0.2/32
 /bin/cat << EOF > /mnt/wg0.conf
 
 [Interface]
-ListenPort = 51820
-PrivateKey = eHjwIsy/goFf=
+PrivateKey = SElck4VwUos0hyXKHxxxxxxxxF6+RIHb9lM9Hgx0Y1g=
 
 [Peer]
-PublicKey = GrJk/5LB0eXRsOsntWg=
-PresharedKey = yUXD4lW+o5MHkvoA1YhZS0=
-Endpoint = 8.xx:51820
+PublicKey = rfA0YJAnSgeHsVcBxxxxxxxxs9ePWpa8s9TSK5wWMl0=
 AllowedIPs = 0.0.0.0/0
+Endpoint = 47.xx.xx.2:PORT
 PersistentKeepalive = 25
-
-EOF
-
 
 
 cat /mnt/wg0.conf
@@ -27,13 +22,14 @@ ip link set up dev wg0
 wg show wg0
 ip route add 0.0.0.0/1 dev wg0
 ip route add 128.0.0.0/1 dev wg0
-ip route add 8.xx/32 via 39.65.220.1 dev ppp0
+ip route add 47.xx.xx.2/32 via 10.x.128.1 dev ppp0
 iptables -t nat -A POSTROUTING -o wg0 -j MASQUERADE
 
 #end
 
 # configure routes
 
+#ip link set down dev wg0
 # ip route del 0.0.0.0/1 dev wg0
 # ip route del 128.0.0.0/1 dev wg0
-# ip route del 8.xx/32 via 39.65.220.1 dev ppp0
+# ip route del 8.208.x.x/32 via 10.x.128.1 dev ppp0

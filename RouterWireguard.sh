76d58da504d085898c0e13ed9742dc16332784ea
happy201807 <143249384+happy201807@users.noreply.github.com>
2024-06-01 22:27:40 +0800

    Update RouterWireguard.sh

--- RouterWireguard.sh
+++ RouterWireguard.sh
@@ -1,14 +1,15 @@
 modprobe wireguard
 ip link add dev wg0 type wireguard
-ip address add dev wg0 172.16.1.2/32
+ip address add dev wg0 10.7.0.3/32
 /bin/cat << EOF > /mnt/wg0.conf
 
 [Interface]
 ListenPort = 51820
-PrivateKey= mH9sW6XoKuxxxxxxxxxxxxxxxxxxxxL6cgh+FiHs=
+PrivateKey = aCzn+YTs0RXi1ivhD6jGJnxLMLYRzZoCvJYuaHQ+alM=
+
 [Peer]
-PublicKey= A5L0CAw9/iqWxxxxxxxxxxxxxxxxx6bLLXHAT9c6zAE=
-Endpoint = xx.xx.xx.xx:10000
+PublicKey = RYfrpS3SlZpUplTGWJXRndkXEMKIEEOaNxSk4b6ruVo=
+Endpoint = ip:2112
 AllowedIPs = 0.0.0.0/0
 PersistentKeepalive = 25
 
@@ -25,5 +26,5 @@ ip link set up dev wg0
 wg show wg0
 ip route add 0.0.0.0/1 dev wg0
 ip route add 128.0.0.0/1 dev wg0
-eval "ip route add xx.xx.xx.xx/32 $(ip route show default 0.0.0.0/0 | sed 's/default //')" dev ppp0
+eval "ip route add ip/32 $(ip route show default 0.0.0.0/0 | sed 's/default //')" dev ppp0
 iptables -t nat -A POSTROUTING -o wg0 -j MASQUERADE
